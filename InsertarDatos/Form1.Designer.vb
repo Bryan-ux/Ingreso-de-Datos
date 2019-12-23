20a7b6332f8cc0e59e543c48f856f7ef9c04ee6c
Bryan-ux <59038942+Bryan-ux@users.noreply.github.com>
2019-12-23 09:49:44 -0500

    Add files via upload

---
+++ InsertarDatos/Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Agregar = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(210, 85)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(21, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(210, 140)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "DESCRIPCION"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(210, 199)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "PRECIO"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(334, 85)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(334, 140)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(300, 22)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(334, 196)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Agregar
+        '
+        Me.Agregar.Location = New System.Drawing.Point(698, 262)
+        Me.Agregar.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Agregar.Name = "Agregar"
+        Me.Agregar.Size = New System.Drawing.Size(133, 28)
+        Me.Agregar.TabIndex = 6
+        Me.Agregar.Text = "AGREGAR"
+        Me.Agregar.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(34, 242)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.Size = New System.Drawing.Size(640, 231)
+        Me.DataGridView1.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("MingLiU_HKSCS-ExtB", 16.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(287, 24)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(267, 28)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "AGREGAR PRODUCTOS"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(698, 333)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "MODIFICAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(698, 397)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 28)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "ELIMINAR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Turquoise
+        Me.ClientSize = New System.Drawing.Size(840, 513)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Agregar)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "PRODUCTOS"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Agregar As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ InsertarDatos/Form1.vb
@@ -0,0 +1,93 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cargarDatos()
+
+    End Sub
+
+    Function cargarDatos()
+        Dim conexion As New SqlConnection(My.Settings.CONEXION)
+        Dim consulta As String
+        consulta = "Select * from productos"
+        Dim query As New SqlCommand(consulta, conexion)
+        Dim adapter As New SqlDataAdapter(query)
+        Dim datostabla As New DataSet
+        adapter.Fill(datostabla, "productos")
+        Me.DataGridView1.DataSource = datostabla.Tables("productos")
+
+
+
+    End Function
+
+    Private Sub Agregar_Click(sender As Object, e As EventArgs) Handles Agregar.Click
+        Dim producto As New Articulo
+        producto.id = CInt(Me.TextBox1.Text)
+        producto.precio = CDbl(Me.TextBox3.Text)
+        producto.descripcion = Me.TextBox2.Text
+
+        agregarDatos(producto)
+        cargarDatos()
+    End Sub
+
+    Function agregarDatos(valor As Articulo)
+        Dim conexion As New SqlConnection(My.Settings.CONEXION)
+        conexion.Open()
+        Dim consulta As String
+        consulta = "insert into productos (Id,Precio,Descripcion) values (" & valor.id & ",'" & valor.precio & "','" & valor.descripcion & "')"
+        Dim query As New SqlCommand(consulta, conexion)
+        query.ExecuteNonQuery()
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim producto As New Articulo
+        producto.id = CInt(Me.TextBox1.Text)
+        producto.precio = CDbl(Me.TextBox3.Text)
+        producto.descripcion = Me.TextBox2.Text
+
+        modificarDatos(producto)
+        cargarDatos()
+    End Sub
+    Function modificarDatos(valor As Articulo)
+        Dim conexion As New SqlConnection(My.Settings.CONEXION)
+        conexion.Open()
+        Dim consulta As String
+        consulta = "update productos set Id= " & valor.id & ",Precio= " & valor.precio & ",Descripcion= '" & valor.descripcion & "' Where Id= " & valor.id & ""
+        Dim query As New SqlCommand(consulta, conexion)
+        query.ExecuteNonQuery()
+    End Function
+    Function eliminarDatos(valor As Articulo)
+        Dim conexion As New SqlConnection(My.Settings.CONEXION)
+        conexion.Open()
+        Dim consulta As String
+        consulta = "delete from productos  Where Id= " & valor.id & ""
+        Dim query As New SqlCommand(consulta, conexion)
+        query.ExecuteNonQuery()
+    End Function
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Dim FilaActual As Integer
+        FilaActual = DataGridView1.CurrentRow.Index
+        TextBox1.Text = DataGridView1.Rows(FilaActual).Cells(0).Value
+        TextBox3.Text = DataGridView1.Rows(FilaActual).Cells(1).Value
+        TextBox2.Text = DataGridView1.Rows(FilaActual).Cells(2).Value
+
+
+
+
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim producto As New Articulo
+        producto.id = CInt(Me.TextBox1.Text)
+        producto.precio = CDbl(Me.TextBox3.Text)
+        producto.descripcion = Me.TextBox2.Text
+
+        eliminarDatos(producto)
+        cargarDatos()
+
+    End Sub
+End Class
